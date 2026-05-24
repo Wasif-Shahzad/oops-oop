7ed99e414b61edccd6accdc3c93a57e23ea5efc7
Wasif Shahzad <wasif_shahzad_1@outlook.com>
2026-05-24 12:53:12 +0500

    Add Register and Login Forms and MVP front-end

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import path
+
+from . import views
+
+app_name = 'core'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.CustomLoginView.as_view(authentication_form=AuthenticationForm), name='login'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from typing import override
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render, reverse
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import UserRegisterForm
+
+def index(request):
+    return render(request, 'core/base.html', {})
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('core:index'))
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            new_user = authenticate(form.cleaned_data['username'], form.cleaned_data['password'])
+            login(request, new_user)
+    else:
+        form = UserRegisterForm()
+    return render(request, 'core/login-register.html', {"form": form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'core/login-register.html'
+    next_page = reverse_lazy('core:index')
+
+    @override
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('core:index'))
+        return super().get(request, *args, **kwargs)
