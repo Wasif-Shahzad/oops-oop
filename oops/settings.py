291e316bef467d6171a2a4f6585ff93c29fc017d
Wasif Shahzad <wasif_shahzad_1@outlook.com>
2026-06-02 12:10:23 +0500

    Make BASE_DIR a Path object

--- oops/settings.py
+++ oops/settings.py
@@ -15,7 +15,7 @@ from environ import Env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 env = Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
