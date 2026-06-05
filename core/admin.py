406f45a288f2b67ba7ff50d11e5bcdd5e985f0d3
Wasif Shahzad <wasif_shahzad_1@outlook.com>
2026-06-05 06:31:21 +0500

    Add filters to django-admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Question, Choice, Level, UserQuiz, UserAnswer
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionModel(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+
+
+class UserQuizModel(admin.ModelAdmin):
+    search_fields = ["user__username"]
+
+
+class UserAnswerModel(admin.ModelAdmin):
+    search_fields = ["user__username"]
+    list_filter = ["is_correct", "timed_out"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Question, QuestionModel)
+admin.site.register(Level)
+admin.site.register(UserQuiz, UserQuizModel)
+admin.site.register(UserAnswer, UserAnswerModel)
