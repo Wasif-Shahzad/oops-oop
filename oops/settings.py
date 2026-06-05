6a576ab3ca8b3dbd3aaf5d6f4324ebfe9242c739
Wasif Shahzad <wasif_shahzad_1@outlook.com>
2026-06-05 07:34:57 +0500

    Add http to CSRF_TRUSTED_ORIGINS AND CORS_ORIGIN_WHITELIST

--- oops/settings.py
+++ oops/settings.py
@@ -34,9 +34,9 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['localhost', '127.0.0.1'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['http://localhost', 'http://127.0.0.1'])
 
-CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST', default=['localhost', '127.0.0.1'])
+CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST', default=['http://localhost', 'http://127.0.0.1'])
 
 # Application definition
 
